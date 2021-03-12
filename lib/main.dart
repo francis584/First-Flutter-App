7c3baa3dd2bcb1774488f4280ecf3033820e8c2e
Francinildo M. Rodrigues <francis584@gmail.com>
2021-03-11 23:17:01 -0300

    first State created

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,46 @@
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(
-    Container(
+  runApp(AppWidget(
+    title: "Fluterando final",
+  ));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
       child: Center(
-          child: Text(
-        'Flutterando',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(color: Colors.white, fontSize: 50.0),
+          child: GestureDetector(
+        child: Text('Contador: $counter'),
+        onTap: () {
+          setState(() {
+            counter++;
+          });
+        },
       )),
-    ),
-  );
+    );
+  }
 }
