a9583f348b7b794ff159238bc4a49934987b6e57
Francinildo M. Rodrigues <francis584@gmail.com>
2021-03-13 14:52:14 -0300

    Singleton state for darkTheme

---
+++ lib/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:first_flutter_project/app_controller.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+          child: Switch(
+              value: AppController.instance.isDartTheme,
+              onChanged: (value) {
+                AppController.instance.changeTheme();
+              })),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          setState(() {
+            counter++;
+          });
+        },
+      ),
+    );
+  }
+}
